3d9533da9d66036cf53b25dca26c26b004e38285
Hnatiak Roman <58102343+Hnatiak@users.noreply.github.com>
2026-06-18 12:40:02 +0200

    Create database entities and repositories

---
+++ src/main/java/org/example/urlshortener/auth/entity/User.java
@@ -0,0 +1,34 @@
+package org.example.urlshortener.auth.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,
+            unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at",
+            nullable = false)
+    private LocalDateTime createdAt;
+}
